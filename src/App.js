24a388a37d3b14de2a95a5af9ee98e9bbe6c4ae3
advaithsshetty <advaith.cs22@sahyadri.edu.in>
2024-06-28 21:12:40 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavigationBar from './Components/Navbar';
+import Home from './pages/Home';
+import Menu from './pages/Menu';
+import OrderHistory from './pages/OrderHistory';
+import Feedback from './pages/Feedback';
+import AdminDashboard from './pages/AdminDashboard';
+import AdminOrderManagement from './pages/AdminManagement';
+import AdminSalesReport from './pages/AdminSalesReport';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <Router>
+    <NavigationBar />
+    <Routes>
+      <Route path="/" exact element={<Home/>} />
+      <Route path="/menu" element={<Menu/>} />
+      <Route path="/order-history" element={<OrderHistory/>} />
+      <Route path="/feedback" element={<Feedback/>} />
+      <Route path="/admin" exact element={<AdminDashboard/>} />
+      <Route path="/admin/order-management" element={<AdminOrderManagement/>} />
+      <Route path="/admin/sales-reports" element={<AdminSalesReport/>} />
+    </Routes>
+  </Router>
+)
 
 export default App;
